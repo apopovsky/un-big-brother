893418855b7f8754c6b0803d970a7d21e8741df8
Ariel Popovsky <apopovsky@gmail.com>
2019-11-28 19:10:52 -0300

    Allow changing subscriber hours. (#46)
    Thank you! I must say that these workflows are really good at expressing the logic of a single command.

--- src/UnTaskAlert/Commands/Workflow/SetSettingsWorkflow.cs
+++ src/UnTaskAlert/Commands/Workflow/SetSettingsWorkflow.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using UnTaskAlert.Common;
 using UnTaskAlert.Models;
 
 namespace UnTaskAlert.Commands.Workflow
 {
     public class SetSettingsWorkflow : CommandWorkflow
     {
+        private const string WorkingHoursSetting = "Workhours";
+        private const string HoursperdaySetting = "HoursPerDay";
+
         enum Steps
         {
             Start = 0,
@@ -14,14 +21,16 @@ namespace UnTaskAlert.Commands.Workflow
             PreferenceValue = 2
         }
 
+        private static readonly string[] SettingNames = {WorkingHoursSetting, HoursperdaySetting};
+        
         protected override async Task<WorkflowResult> PerformStep(string input, Subscriber subscriber, long chatId)
         {
             string settingName = null, settingValue = null;
             if (CurrentStep == (int) Steps.Start)
             {
+                var settingsList = SettingNames.Aggregate("", (prev, next) => prev + "-" + next + Environment.NewLine);
                 await Notifier.Respond(chatId, $"Please enter one of the following preferences to change:{Environment.NewLine}" +
-                                               $"-Setting1{Environment.NewLine}" +
-                                               $"-Setting2{Environment.NewLine}" +
+                                               $"{settingsList}" +
                                                $"You can send setting and value together using this format: SettingX=value");
 
                 CurrentStep = (int) Steps.PreferenceName;
@@ -32,7 +41,7 @@ namespace UnTaskAlert.Commands.Workflow
             {
                 var inputParts = input.Split('=', StringSplitOptions.RemoveEmptyEntries);
                 settingName = inputParts[0];
-                if (string.IsNullOrWhiteSpace(settingName))
+                if (!IsSettingNameValid(settingName))
                 {
                     await Notifier.Respond(chatId, "Please provide a valid setting name");
                     
@@ -43,7 +52,8 @@ namespace UnTaskAlert.Commands.Workflow
                 {
                     Data = settingName;
                     CurrentStep = (int) Steps.PreferenceValue;
-                    await Notifier.Respond(chatId, "Please provide a new setting value");
+
+                    await Notifier.Respond(chatId, $"Please provide a new setting value. Actual={GetActualSetting(subscriber, settingName)}");
 
                     return WorkflowResult.Continue;
                 }
@@ -56,16 +66,74 @@ namespace UnTaskAlert.Commands.Workflow
                 settingValue = input;
             }
 
-            await Notifier.Respond(chatId, $"From now on {settingName}={settingValue}");
-            Logger.LogInformation($"Changing setting {settingName}. New value: {settingValue}");
+            var success = TryChangeSetting(subscriber, settingName, settingValue, chatId);
+			if (success)
+            {
+                await Notifier.Respond(chatId, $"From now on {settingName}={settingValue}");
+                Logger.LogInformation($"Changing setting {settingName}. New value: {settingValue}");
+            
+                return WorkflowResult.Finished;
+            }
+            else
+            {
+                await Notifier.Respond(chatId, $"Could not change {settingName}={settingValue}. Please try again.");
+                CurrentStep = (int) Steps.PreferenceName;
 
-            return WorkflowResult.Finished;
+				return WorkflowResult.Continue;
+            }
+		}
+
+        private string GetActualSetting(Subscriber subscriber, string settingName)
+        {
+            switch (settingName)
+            {
+				case HoursperdaySetting:
+                    return subscriber.HoursPerDay.ToString();
+				case WorkingHoursSetting:
+                    return $"{subscriber.StartWorkingHoursUtc:hh\\:mm}-{subscriber.EndWorkingHoursUtc:hh\\:mm}";
+				default:
+                    return string.Empty;
+            }
+        }
+
+        private bool IsSettingNameValid(string settingName)
+        {
+            return !string.IsNullOrWhiteSpace(settingName) || SettingNames.Contains(settingName);
+        }
+
+        private bool TryChangeSetting(Subscriber subscriber,string settingName, string settingValue, long chatId)
+        {
+            bool success = false;
+            if (settingName == HoursperdaySetting)
+            {
+                if(int.TryParse(settingValue, out var intValue))
+                {
+                    subscriber.HoursPerDay = Convert.ToInt32(intValue);
+                    success = true;
+                }
+            }
+            else if (settingName == WorkingHoursSetting)
+            {
+                var values = settingValue.Split(new[]{'-',' '},StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length == 2)
+                {
+                    var startProvided = TimeSpan.TryParseExact(values[0], "hh\\:mm", CultureInfo.InvariantCulture, TimeSpanStyles.None, out var startValue);
+                    var endProvided = TimeSpan.TryParseExact(values[1], "hh\\:mm", CultureInfo.InvariantCulture, TimeSpanStyles.None, out var endValue);
+                    if (startProvided && endProvided && endValue > startValue)
+                    {
+                        subscriber.StartWorkingHoursUtc = startValue;
+                        subscriber.EndWorkingHoursUtc = endValue;
+                        success = true;
+                    }
+                }
+            }
+
+            return success;
         }
 
         protected override void InjectDependencies(IServiceProvider serviceProvider)
         {
-            // no-op
-        }
+		}
 
         protected override bool DoesAccept(string input)
         {
